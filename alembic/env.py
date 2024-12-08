6a8470e16494ecfe61becc2203cb3a710cd86405
Juan Carlos <jkloaiza19@gmail.com>
2024-12-08 07:58:13 -0600

    Update alembic config

--- alembic/env.py
+++ alembic/env.py
@@ -22,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = get_declarative_base().get_declarative_base()
+target_metadata = get_declarative_base().get_model_base()
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
